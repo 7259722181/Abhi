8d6922da00034206d68162719287132b1eac7f02
7259722181 <110157902+7259722181@users.noreply.github.com>
2025-05-19 11:41:32 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,13 @@
+a=(input())
+# print("Enter your name: ")
+b ="Abhishek"
+c ="abhishek"
+e = "ABHISHEK"
+if a==b:
+    print("Wait while your account is loading")
+else:
+    print("Sorry your name doesn't match")
+
+    
+
+
