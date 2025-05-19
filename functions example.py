b011ad10777c505128f283764704ccf28d146bb0
unknown <abhishek.b.p01@gmail.com>
2025-05-19 10:08:46 +0530

    Adding file Functions

---
+++ functions example.py
@@ -0,0 +1,14 @@
+def add_numbers(n1,n2):
+    return n1+n2
+  
+
+def multiply_numbers(n1,n2):
+   return n1*n2
+   
+    
+
+r1=add_numbers(5,6)
+r2=multiply_numbers(5,6)
+print("Addition of 2 numbers:",r1)
+print("Multiplication of 2 numbers:",r2)
+
